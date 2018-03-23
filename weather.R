7d3d12fba1c445b61576451f04e31a3f9542cf1e
Hauncher <samanthactoet@gmail.com>
2018-03-23 13:11:29 -0400

    weather data

---
+++ weather.R
@@ -0,0 +1,17 @@
+# Import external weather data about Charlottesville
+# samanthactoet@gmail.com
+
+
+
+install.packages("rwunderground")
+
+# Documentation: https://cran.r-project.org/web/packages/rwunderground/rwunderground.pdf
+
+key <- rwunderground::set_api_key("98424ba247eed14b")
+
+location <- rwunderground::set_location(zip_code = "22902")
+
+weather <- rwunderground::history_range(location, date_start = "20170101", date_end = "20171227")
+
+
+
